005d53936a4bc27e35ee321f9d16941b18e191d6
patilaniruddha98 <66047669+patilaniruddha98@users.noreply.github.com>
2023-01-30 09:13:41 +0530

    sg.tf

---
+++ securityGroup.tf
@@ -0,0 +1,50 @@
+module "bastion_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "bastion_sg"
+  description = "bastion security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      cidr_blocks = "0.0.0.0/0" 
+    }
+  ]
+   
+   egress_rules = ["all-all"]
+  tags = {
+    Name = "ani_allow_ssh"
+  }
+}
+
+module "private_instance_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "private_instance_sg"
+  description = "private instance security group"
+  vpc_id      = module.vpc.vpc_id
+
+ 
+   computed_ingress_with_source_security_group_id = [
+    {
+      rule                     = "private_instance_sg"
+      source_security_group_id = module.bastion_sg.security_group_id
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      source_security_group_id = module.bastion_sg.security_group_id
+    },
+  ]
+   
+   egress_rules = ["all-all"]
+  tags = {
+    Name = "ani_allow_all_tcp"
+  }
+}
